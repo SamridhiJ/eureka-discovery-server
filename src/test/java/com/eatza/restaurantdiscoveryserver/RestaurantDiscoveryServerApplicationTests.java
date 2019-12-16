3e98d4579bf8e684a3927522897afe5210c4bdca
Samridhi Jain <M1050868@mindtree.com>
2019-12-16 10:56:49 +0530

    service registry

---
+++ src/test/java/com/eatza/restaurantdiscoveryserver/RestaurantDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatza.restaurantdiscoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
